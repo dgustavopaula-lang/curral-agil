6210d80ab046a48c030527f699df154ed0c48379
Gustavo Paula <SEU_EMAIL_DO_GITHUB>
2026-09-21 16:32:40 -0300

    Finaliza instalacao PWA do Curral Agil

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "curral-agil-v1";
+const CACHE = "curral-agil-v2";
 
 const ARQUIVOS = [
   "./",
@@ -6,23 +6,25 @@ const ARQUIVOS = [
   "./css/app.css",
   "./js/app.js",
   "./js/db.js",
-  "./manifest.webmanifest"
+  "./manifest.webmanifest",
+  "./icons/favicon-32.png",
+  "./icons/apple-touch-icon.png",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/icon-maskable-512.png"
 ];
 
 self.addEventListener("install", event => {
-
   event.waitUntil(
     caches.open(CACHE)
       .then(cache => cache.addAll(ARQUIVOS))
   );
 
   self.skipWaiting();
-
 });
 
 
 self.addEventListener("activate", event => {
-
   event.waitUntil(
     caches.keys().then(chaves =>
       Promise.all(
@@ -34,19 +36,35 @@ self.addEventListener("activate", event => {
   );
 
   self.clients.claim();
-
 });
 
 
 self.addEventListener("fetch", event => {
 
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  if (event.request.mode === "navigate") {
+
+    event.respondWith(
+      fetch(event.request)
+        .then(resposta => resposta)
+        .catch(() => caches.match("./index.html"))
+    );
+
+    return;
+  }
+
   event.respondWith(
-
     caches.match(event.request)
-      .then(resposta =>
-        resposta || fetch(event.request)
-      )
+      .then(cacheado => {
 
+        if (cacheado) {
+          return cacheado;
+        }
+
+        return fetch(event.request);
+      })
   );
-
 });
